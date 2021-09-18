1d5dd111cfb30530bb75c48ca3e1064dbb0bc682
Vasanth Reddy Baddam <vasanth@vasanths-mbp.myfiosgateway.com>
2021-09-18 09:47:18 -0400

    added changes to css

--- src/info.js
+++ src/info.js
@@ -7,7 +7,7 @@ function Info (){
   
         <h2 className="about-me">About Me</h2>
         <p className="about-me">
-        Graduated from BTech in electrical engineering from the National Institute of Technology in Jaipur (2019), I am currently pursuing a phD in Computer Science at Virginia Tech university. Being monitored by <a className="links" href="https://people.cs.vt.edu/hdardiry/">
+        Graduated from BTech in electrical engineering from the National Institute of Technology in Jaipur (2019), I am currently pursuing a PhD in Computer Science at Virginia Tech university. Being monitored by <a className="links" href="https://people.cs.vt.edu/hdardiry/">
             Dr. Hoda Eldardiry"
           </a>{" "} and <a className="links" href="https://ece.vt.edu/people/profile/boker">
             Dr. Almuatazbellah (Muataz) Boker"
